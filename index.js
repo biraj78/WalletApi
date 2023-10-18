7c36783f06f95e6ce6a9e63ed3192f70869a569e
biraj78 <123976149+biraj78@users.noreply.github.com>
2023-10-18 21:18:59 +0545

    sfsd

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const indexHandelar = require("./handelar/indexHandelar");
 const createMovies = require("./handelar/createMovies");
 const getMovies = require("./handelar/getMovies");
-const path = require("path")
-// const cors = require('cors');
+const path = require("path");
+const cors = require("cors");
 
 require("dotenv").config();
 // model import
@@ -13,8 +13,8 @@ require("./movies.model");
 
 const app = express();
 
-app.use(express.json())
-// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 const mongo_connect = process.env.mongo_connect;
 
 mongoose
@@ -31,20 +31,16 @@ app.get("/", indexHandelar);
 //more routes
 app.post("/movies", createMovies);
 
-app.get("/movies", getMovies)
+app.get("/movies", getMovies);
 
-app.use(express.static(path.join(__dirname, './agency_web/build')))
+app.use(express.static(path.join(__dirname, "./agency_web/build")));
 
-app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, './agency_web/build/index.html'))
-})
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./agency_web/build/index.html"));
+});
 
-const PORT = process.env.PORT || 8000
-
-
-
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log("server started");
 });
-
